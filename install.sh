216525921f78afd3ea0d2ba6db9c313bd80bcd8d
im-ng <2039564+im-ng@users.noreply.github.com>
2025-11-01 17:12:37 +0530

    corrections

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'Anubar installer'
+echo 'Anu installer'
 
 DP=$([[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]] && echo true || echo false)
 if [ $DP == false ]; then
@@ -18,6 +18,7 @@ echo -e 'Start default upgrade'
 sudo apt install curl wget git -y
 sudo apt install chrome-gnome-shell -y
 sudo apt install gnome-browser-connector -y
+sudo apt install gnome-shell-extension-manager
 echo -e ''
 sleep 1
 
@@ -71,12 +72,13 @@ gnome-extensions install appindicatorsupportrgcjonas.gmail.com.v58.shell-extensi
 echo -e 'Download necessary gnome apps'
 wget https://github.com/Ulauncher/Ulauncher/releases/download/5.15.7/ulauncher_5.15.7_all.deb
 sudo apt install ./ulauncher_5.15.7_all.deb -y
+mkdir ~/.config/autostart
 cp config/ulauncher.desktop ~/.config/autostart/
 echo -e ''
 
 echo - e 'Load adwaita blue theme'
 git clone https://github.com/ricardoherreramx/adwaitaru.git
-mkdir .icons
+mkdir ~/.icons
 cp -r adwaitaru/Adwaitaru-blue .icons/
 echo -e ''
 
@@ -85,6 +87,21 @@ dconf load / < config/dconf-settings.ini
 echo -e ''
 sleep 1
 
+echo -e 'Install zig'
+wget https://ziglang.org/download/0.15.1/zig-x86_64-linux-0.15.1.tar.xz
+sudo tar -C /usr/local/ -xf zig-x86_64-linux-0.15.1.tar.xz 
+sudo mv /usr/local/zig-x86_64-linux-0.15.1.tar.xz /usr/local/zig
+echo 'export ZIG=/usr/local/zig' >> ~/.bashrc
+echo 'export PATH=$ZIG:$PATH' >> ~/.bashrc
+echo -e ''
+
+echo -e 'Install Go'
+wget https://go.dev/dl/go1.25.3.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.25.3.linux-amd64.tar.gz
+echo 'export GO=/usr/local/go' >> ~/.bashrc
+echo 'export PATH=$GO:$PATH' >> ~/.bashrc
+echo -e ''
+
 echo -e 'Remove downloaded dependencies'
 rm openbarneuromorph.v35.shell-extension.zip
 rm tilingshellferrarodomenico.com.v12.shell-extension.zip
@@ -92,12 +109,14 @@ rm VitalsCoreCoding.com.v68.shell-extension.zip
 rm appindicatorsupportrgcjonas.gmail.com.v58.shell-extension.zip
 rm ulauncher_5.15.7_all.deb
 rm -rf adwaitaru
+rm -rf zig-x86_64-linux-0.15.1.tar.xz
+rm -rf go1.25.3.linux-amd64.tar.gz
 echo -e ''
 
 echo -e "Clean up done"
 sleep 1
 
-echo -e "Anubar installation completed"
+echo -e "Anu installation completed"
 sleep 2
 
 gnome-session-quit --force
--- uninstall.sh
+++ uninstall.sh
@@ -3,11 +3,33 @@
 echo -e 'Restoring default Gnome session'
 echo -e ''
 
+echo -e 'Uninstall codium depedencies'
+codium --uninstall-extension akamud.vscode-theme-onelight
+codium --uninstall-extension catppuccin.catppuccin-vsc
+codium --uninstall-extension catppuccin.catppuccin-vsc-icons
+codium --uninstall-extension vadimcn.vscode-lldb
+codium --uninstall-extension ms-azuretools.vscode-containers
+codium --uninstall-extension ms-azuretools.vscode-docker
+codium --uninstall-extension golang.go
+codium --uninstall-extension ziglang.vscode-zig
+codium --uninstall-extension dreamcatcher45.podmanager
+codium --uninstall-extension cweijan.vscode-redis-client
+codium --uninstall-extension mtxr.sqltools
+codium --uninstall-extension vscode-icons-team.vscode-icons
+codium --uninstall-extension antfu.icons-carbon
+codium --uninstall-extension alexdauenhauer.catppuccin-noctis-icons
+codium --uninstall-extension donjayamanne.githistory
+codium --uninstall-extension shd101wyy.markdown-preview-enhanced
+codium --uninstall-extension esbenp.prettier-vscode
+echo -e ''
+
 echo -e 'Removing dependency softwares'
 sudo apt autoremove wget -y
 sudo apt autoremove chrome-gnome-shell -y
 sudo apt autoremove gnome-browser-connector -y
 sudo apt autoremove ulauncher -y
+sudo apt autoremove gnome-shell-extension-manager -y
+sudo apt autoremove codium -y
 echo -e ''
 sleep 1
 
@@ -20,13 +42,16 @@ echo -e ''
 sleep 1
 
 echo -e 'Restoring Gnome default'
+rm -rf ~/.icons
 rm ~/.config/dconf/user
 rm ~/.config/autostart/ulauncher.desktop
 rm -r ~/.config/ulauncher
+rm -r /usr/local/go
+rm -rf /usr/local/zig
 echo -e ''
 sleep 1
 
-echo -e 'Anubar uninstallation completed'
+echo -e 'Anu uninstallation completed'
 echo -e ''
 sleep 2
 
